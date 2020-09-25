193743477a4b460ed446c6de7390d75af1480cd8
BonDah18 <proctorine@gmail.com>
2020-09-25 13:47:42 -0700

    adding css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
-import logo from './logo.svg'
+import logo from './logo.svg';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-    <div className="App">
-      <header className="App-header">Hello StackBlitz!</header>
-      <img src={logo} className="App-logo alt="logo" />
-      <p>Edit <code>src/App.js</code> and save to reload. 
-      </p>
-      <a
-      className="App-link"
-      href="https://reactjs.org"
-    </div>
-      );
+      <div className="container">
+      <div className="row">
+      <div className="col-6">column number one</div>
+      <div className="col-6">
+      <span> 
+      <i className="fas fa-home"/>
+      </span> 
+      </div>
+      </div>
+      </div>
+
+    );
   }
 }
   
