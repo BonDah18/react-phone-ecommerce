5d5229b1c11ecfa3a7b78096aae67db1f7bd8f66
BonDah18 <proctorine@gmail.com>
2020-09-24 17:14:25 -0700

    8 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./style.css";
+import React, { Component } from "react";
+import logo from './logo.svg'
+import "./App.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <header className="App-header">Hello StackBlitz!</header>
+      <img src={logo} className="App-logo alt="logo" />
+      <p>Edit <code>src/App.js</code> and save to reload. 
+      </p>
+      <a
+      className="App-link"
+      href="https://reactjs.org"
     </div>
-  );
+      );
+  }
 }
+  
+
+
+export default App
