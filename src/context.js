3d2ec8c3f8297a0d707195c24286421aa2823fed
BonDah18 <proctorine@gmail.com>
2020-09-30 12:58:20 -0700

    details and cart addition

--- src/context.js
+++ src/context.js
@@ -8,7 +8,13 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state ={
     products: [], 
-    detailProduct:detailProduct
+    detailProduct: detailProduct,
+    cart: [],
+    modalOpan: false, 
+    modalProduct: detailProduct, 
+    cartSubTotal: 0,
+    cartTax:0,
+    cartTotal: 0
   };
   componentDidMount(){
     this.storeProducts();
@@ -19,25 +25,76 @@ class ProductProvider extends Component {
       const singleItem = {...item};
       tempProducts = [...tempProducts, singleItem];
 
-    })
+    });
     this.setState(()=>{
       return{products:tempProducts};
+    });
+  };
+
+getItem = (id) =>{
+  const product = this.state.products.find(item => item.id ===id);
+  return product;
+}
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(()=>{
+      return {detailProduct:product}
     })
-  }
-  handleDetail = () => {
-    console.log('hello from detail');
-
-  }
-  addToCart = () => {
-    console.log(`hello from add to cart.id is ${id}`);
-
-  }
+  };
+  addToCart = id => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index]
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price; 
+    this.setState(()=>{
+      return { products: tempProducts,cart:[this.state.cart, product] };
+    },()=>{console.log(this.state)
+    });
+  };
+    openModal = id =>{
+      const product = this.getItem(id);
+      this.setState(()=>{
+        return {modalProduct:product, modalOpen:true};
+      });
+    };
+    closeModal = () => {
+      this.setState(()=>{
+        return {modalOpen:false}
+      });
+    };
+    increment = (id) =>{
+      console.log('this is increment method');
+    }
+    decrement = (id) =>{
+      console.log('this is decrement method');
+    }
+    removeItem = (id) =>{
+      console.log('item removed');
+    }
+    clearCart = () =>{
+      console.log('cart was cleared');
+    }
+    addTotals = () => {
+      let subTotal = 0;
+      
+    }
   render() {
     return (
-      <ProductContext.Provider value={{
+      <ProductContext.Provider 
+        value={{
         ...this.state,
         handleDetail:this.handleDetail,
         addToCart:this.addToCart,
+        openModal:this.openModal,
+        closeModal:this.closeModal,
+        increment:this.increment,
+        decrement:this.decrement,
+        removeItem:this.removeItem,
+        clearCart:this.clearCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
