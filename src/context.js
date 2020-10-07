da3367049cb0dcfbbbb75a096eb91e2e0964436e
BonDah18 <proctorine@gmail.com>
2020-10-07 14:11:10 -0700

    updates

--- src/context.js
+++ src/context.js
@@ -75,7 +75,7 @@ getItem = (id) => {
       const index = tempCart.indexOf(selectedProduct);
       const product = tempCart[index];
 
-      product.count = product.count = 1;
+      product.count = product.count + 1;
       product.total = product.count * product.price;
 
       this.setState(
